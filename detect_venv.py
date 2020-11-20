a30ee99579ab49f78230439352009a76d5a43e9a
Tuhin Mitra <tuhinmitra190221@gmail.com>
2020-11-20 23:20:26 +0530

    v_1.0.0

---
+++ detect_venv.py
@@ -0,0 +1,56 @@
+import multiprocessing
+import time
+import os
+import timeit
+
+def write_in_file(filename, root):
+    venv_name = os.path.basename(root)
+    with open(filename, 'a') as text_file:
+        text_file.write(venv_name + ': '+root+'\n')
+
+def match_feature(file_name, dir_path):
+    # this is the check pattern for windows only
+    checks = ["Lib", "Scripts/activate.ps1", "Scripts/activate.bat"]
+    # will update the check pattern for Linux soon...
+    
+    check_flag = True
+    for locs in checks:
+        path = os.path.realpath(os.path.join(dir_path, locs))
+        if not os.path.exists(path):
+            check_flag = False  # if any of the path is missing, then discard the folder
+    if check_flag:
+        print(f"Windows 10 venv found, name:{os.path.basename(dir_path)}")
+        write_in_file(file_name, dir_path)
+
+def main_runner(dir_path):
+    file_name = 'test_op_'+ os.path.basename(dir_path) + '.txt'
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    print("Checking:",os.path.basename(dir_path))
+    
+    for root, dirs, files in os.walk(dir_path):
+        if os.path.isdir(root):
+            for dir in dirs.copy():
+                if dir.startswith('_') or dir.startswith('.'):
+                    dirs.remove(dir)
+            match_feature(file_name, root)
+    
+
+
+if __name__ == '__main__':
+    count = int(input("Enter number of parallel searches:"))
+    check_paths =[]
+    
+    for i in range(count):
+        check_path = input("Enter checking path:")
+        check_paths.append(check_path)
+    print(check_paths,'\n')
+    time.sleep(1.0)
+    a = timeit.default_timer()
+    with multiprocessing.Pool() as pool:
+        for check_path in check_paths:
+            check_path = os.path.realpath(check_path)
+            print("searching in path:", check_path)
+            pool.map(main_runner, (check_path,))
+    b = timeit.default_timer()
+    print("Time taken:", (b-a), " seconds")
